2b0e3f24b6e26491808e9ac7308fedd7db860d0f
jairogp <jairogp@users.noreply.github.com>
2017-05-07 04:01:26 -0300

    Add files via upload

---
+++ Exemplo_Sensor_DHT11_ou_DHT22_e_LCD_I2C/Exemplo_Sensor_DHT11_ou_DHT22_e_LCD_I2C.ino
@@ -0,0 +1,77 @@
+    /*
+    Pinagem do LCD
+    
+    GND => (-)
+    VCC => 5V
+    SDA => A4
+    SCL => A5 
+    
+  
+    Pinagem do DHT11 Olhando de frente
+
+    Pin 1 => VCC
+    Pin 2 => Pin 8 do Arduino
+    Pin 3 => Sem Uso
+    Pin 4 => GND
+    */
+
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h> 
+#include <DHT.h> 
+
+LiquidCrystal_I2C lcd(39,16,2);  // 0x27 ou 39
+
+//Define a ligação ao pino de dados do sensor
+#define DHTPIN 8
+ 
+//Define o tipo de sensor DHT utilizado
+#define DHTTYPE DHT11
+ 
+DHT dht(DHTPIN, DHTTYPE);
+ 
+ 
+//Array simbolo grau
+byte grau[8] ={ B00001100,
+                B00010010,
+                B00010010,
+                B00001100,
+                B00000000,
+                B00000000,
+                B00000000,
+                B00000000,};
+ 
+void setup()
+{
+lcd.init();                // initialize the lcd 
+lcd.backlight();           // acende a luz de fundo no lcd
+lcd.createChar(0, grau);   //Cria o caractere customizado com o simbolo do grau
+}
+ 
+void loop()
+{
+float h = dht.readHumidity(); //Le o valor da umidade
+float t = dht.readTemperature(); //Le o valor da temperatura
+lcd.setCursor(0,0);
+lcd.print("Temp : ");
+lcd.print(" ");
+lcd.setCursor(7,0);
+lcd.print(t,1);
+lcd.setCursor(12,0);
+ 
+//Mostra o simbolo do grau formado pelo array
+lcd.write((byte)0);
+ 
+//Mostra o simbolo do grau quadrado
+//lcd.print((char)223);
+ 
+lcd.setCursor(0,1);
+lcd.print("Umid : ");
+lcd.print(" ");
+lcd.setCursor(7,1);
+lcd.print(h,1);
+lcd.setCursor(12,1);
+lcd.print("%");
+ 
+//Intervalo recomendado para leitura do sensor
+delay(2000);
+}
---
+++ Exemplo_Sensor_DHT11_ou_DHT22_na_Serial/Exemplo_Sensor_DHT11_ou_DHT22_na_Serial.ino
@@ -0,0 +1,54 @@
+  /*
+  Pinagem do DHT11 Olhando de frente
+  Pin 1 => VCC
+  Pin 2 => Pin 7 do Arduino
+  Pin 3 => Sem Uso
+  Pin 4 => GND
+  */
+  #include <DHT.h>
+
+  // Pino conectado ao pino de dados do sensor
+  #define DHTPIN 7
+
+  // Utilize a linha abaixo de acordo com o modelo do sensor
+  //#define DHTTYPE DHT11   // Sensor DHT11
+  #define DHTTYPE DHT22   // Sensor DHT 22  (AM2302)
+  //#define DHTTYPE DHT21   // Sensor DHT 21 (AM2301)
+
+  // Definicoes do sensor : pino, tipo
+  DHT dht(DHTPIN, DHTTYPE);
+
+  void setup() 
+  {
+  Serial.begin(9600); 
+  Serial.println("Aguardando dados...");
+  // Iniclaiza o sensor DHT
+  dht.begin();
+  }
+
+  void loop() 
+  {
+  // Aguarda 5 segundos entre as medicoes
+  delay(5000);
+  
+  // Leitura da umidade
+  float h = dht.readHumidity();
+  // Leitura da temperatura (Celsius)
+  float t = dht.readTemperature();
+  
+  // Verifica se o sensor esta respondendo
+  if (isnan(h) || isnan(t))
+  {
+    Serial.println("Falha ao ler dados do sensor DHT !!!");
+    return;
+  }
+
+  // Mostra a temperatura no serial monitor e no display
+  Serial.print("Temperatura: "); 
+  Serial.print(t);
+  Serial.print(" graus  ");
+  // Mostra a umidade no serial monitor e no display
+  Serial.print("Umidade: "); 
+  Serial.print(h);
+  Serial.println(" %");
+  }
